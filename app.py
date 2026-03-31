d5b627a68abadf9de4d10aaa072073fe238aa480
Jeferson <jeffim.oliveira@gmail.com>
2026-03-30 23:10:24 -0400

    fix porta render

--- app.py
+++ app.py
@@ -68,5 +68,8 @@ def consultar():
 
     return jsonify(resultado)
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
